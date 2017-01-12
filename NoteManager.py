39b651db96ef614a78ee7d49fe1cb08b6dbe9f45
JulienBuonarota <j.buohk@gmail.com>
2017-01-12 15:04:03 +0100

    main core features are done

--- NoteManager.py
+++ NoteManager.py
@@ -4,7 +4,14 @@ from WordObject import *
 WL = WordList(ListCour)
 WL.Compile()
 WL.SetVerbose("Definition", True)
+WL.SetVerbose("Theoreme", True)
 WL.SetVerbose("Chapitre", True)
 WL.SetVerbose("SousChapitre", True)
-WL.Display("../Cours/Topologie.txt")
-
+WL.SetVerbose("Date", True)
+cour = "../Cours/Topologie.txt"
+cour2 = "../Cours/Test.txt"
+#WL.Display(cour)
+print "----------"
+#WL.DisplayDate(cour, "29/09/2016")
+#WL.DisplayLast(cour)
+WL.Search(cour, "Boule")
--- WordObject.py
+++ WordObject.py
@@ -12,14 +12,14 @@ class WordList(object):
         
         
     def __repr__(self):
-        return str(self.l)
+        return str(self.d)
 
     def SetVerbose(self, name, verbose):
         for i in self.d.iterkeys():
                 if i == name:
                     self.d[i]["Verbose"] = verbose
                     break
-                
+    
     def Compile(self):
         for i in self.d.itervalues():
             i["Word"].Compile()
@@ -27,16 +27,93 @@ class WordList(object):
     def Display(self, file):
         with open(file, 'r') as Course:
             s = ""
-            for i in Course:
-                for j in self.d.itervalues():
-                    if j["Verbose"] and j["Word"].Test(i):
-                        s = s + j["Word"].Display(i)
+            for line in Course:
+                for i in self.d.itervalues():
+                    if i["Verbose"] and i["Word"].Test(line):
+                        s = s + i["Word"].Display(line)
                         break
             print s
 
+    def NbPages(self, file):
+        with open(file, 'r') as Course:
+            nb = 0
+            for line in Course:
+                if self.d["Page"]["Word"].Test(line):
+                    nb = nb + 1
+        return nb
+
+    def DisplayDate(self, file, date):
+        with open(file, 'r') as Course:
+            s = ""
+            tmp = False
+            for line in Course:
+                if self.d["Date"]["Word"].Test(line):
+                    if line == (date + "\n"):
+                        tmp = True
+                    else:
+                        tmp = False
+                if tmp:
+                    for i in self.d.itervalues():
+                        if i["Verbose"] and i["Word"].Test(line):
+                            s = s + i["Word"].Display(line)
+                            break
+            print s   
+
+    def DisplayLast(self, file):
+        """
+        Display the last entry of the file vis-a-vis to the dates of the entries
+        """
+        with open(file, 'r') as Course:
+            tmp = []
+            for line in Course:
+                if self.d["Date"]["Word"].Test(line):
+                    tmp.append(line)
+        #sorting the dates
+        tmp = [[re.sub("\n", "", j) for j in re.split("[ \/]", i)] for i in tmp]
+        top = tmp[0]
+        for i in tmp[1:]:
+            b1 = i[2] >= top[2]
+            b2 = i[1] >= top[1]
+            b3 = i[0] > top[0]
+            if b1 and b2 and b3:
+                top = i
+        date = top[0] + "/" + top[1] + "/" + top[2]
+        self.DisplayDate(file, date)
+
+    def Search(self, file, var):
+        """
+        Basic search for first occurence of var in file
+        """
+        with open(file, 'r') as Course:
+            tmpDate = ""
+            tmpChapter = ""
+            tmp = ""
+            for line in Course:
+                if self.d["Date"]["Word"].Test(line):
+                    tmpDate = line
+                    continue
+                elif self.d["Chapitre"]["Word"].Test(line):
+                    tmpChapter = line
+                    continue
+
+                #removing beginning key word from line
+                line = line.split(" ")
+                line.pop(0)
+                if len(line) == 0:
+                    continue
+                s = line[0]
+                for i in line[1:]:
+                    s = s + " " + i
+
+                #match
+                line = s
+                if re.match(var, line):
+                    tmp = line
+                    break
+            print tmpDate + tmpChapter + tmp
+                        
 class Word(object):
     Indent = ""
-    Column = 0
     def __init__(self, re, name, List):
         self.Re = re
         self.Name = name
@@ -61,14 +138,12 @@ class Word(object):
         else:
             pass
         
-    def Test(self, ligne):
-        if self.Re.match(ligne):
+    def Test(self, line):
+        if self.Re.match(line):
             return True
         else:
             return False
 
-
-    
 #Specification of display for each individual words defined
 ListCour = []
 Chapitre = Word('\\* ', "Chapitre", ListCour)
@@ -114,4 +189,4 @@ Note = Word('Note', "Note", ListCour)
 Corollaire = Word('Cor', "Corollaire", ListCour)
 Retour = Word('\\n', "Retour", ListCour)
 Page = Word('#', "Page", ListCour)
-
+Date = Word("[0-9]{1,2}[/ ]([0-9]{1,2}|[A-Za-z]{3,8})[/ ][0-9]{2,4}", "Date", ListCour)
